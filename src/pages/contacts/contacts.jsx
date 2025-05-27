83b078af24493b8c9d8c2c4da7e895aac164e36f
NuriddinJamshed <j.nuriddin2021@gmail.com>
2025-05-27 21:51:51 +0500

    Client Side created

---
+++ src/pages/contacts/contacts.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Contacts = () => {
+  return (
+    <div>
+      <h1 className='text-[#808080] my-[30px] w-[90%] m-auto text-[18px]'>
+        Home /<span className='text-[#000]'>Cantact</span>
+      </h1>
+      <div>
+        <div>
+          <div>
+            <div>
+
+              <p>Call To Us</p>
+            </div>
+            <p>We are available 24/7, 7 days a week.
+              Phone: +8801611112222</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Contacts
